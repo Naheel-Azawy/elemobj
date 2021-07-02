66518825a4f75b0c45b267b6cf4d84b32049c054
Naheel-Azawy <naheelazawy@gmail.com>
2021-07-02 21:22:22 +0300

    more cleanups

--- example.js
+++ example.js
@@ -1,42 +1,44 @@
 Object.assign(document.body.style, {
     background: "#050505",
-    color: "white",
+    color:      "white",
     fontFamily: "sans-serif"
 });
 
-let counter;
 document.body.appendChild($div({
     style: {
         textAlign: "center",
-        maxWidth: "700px",
-        margin: "auto"
+        maxWidth:  "700px",
+        margin:    "auto"
     },
     content: [
         $h1("elemobj"),
 
-        $p({style: {textAlign: "left"}}, "Elements object, create javascript DOM elements in a nicer way. Kinda like if html and js were blended together. It's still js syntax though, we don't speak jsx here."),
+        $p({style: {textAlign: "left"}},
+           "Elements object, create javascript DOM elements in a nicer way. " +
+           "Kinda like if html and js were blended together. " +
+           "It's still js syntax though, we don't speak jsx here."),
 
         $table({
             style: {
-                width: "100%",
-                margin: "auto",
-                background: "#ffffff11",
-                padding: "10px",
+                width:        "100%",
+                margin:       "auto",
+                background:   "#ffffff11",
+                padding:      "10px",
                 borderRadius: "10px",
-                boxShadow: "0px 1px #000000ff"
+                boxShadow:    "0px 1px #000000ff"
             },
             content: [
                 $tr([
                     $td("COUNTER:"),
-                    counter = $span("0")
+                    $td($span({id: "counter"}, "0"))
                 ]),
 
                 $tr([
                     $td("BUTTON:"),
-                    $button({
-                        onclick: event => counter.innerText++,
+                    $td($button({
+                        onclick: event => ++$get("#counter").innerText,
                         run: it => console.log(`added a new ${it.tagName}`)
-                    }, "CLICKY")
+                    }, "CLICKY"))
                 ])
             ]
         }),
